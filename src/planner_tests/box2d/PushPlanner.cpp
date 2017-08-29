1e2ea62a18bbb67e9d2c6053cd6dc27b89c9b592
Joshua Haustein <haustein@kth.se>
2017-08-29 14:35:00 +0200

    Added some test worlds

--- src/planner_tests/box2d/PushPlanner.cpp
+++ src/planner_tests/box2d/PushPlanner.cpp
@@ -10,9 +10,10 @@ using namespace planner_tests::box2d;
 
 int main(int argc, char **argv) {
     sim_env::Box2DWorldPtr world = std::make_shared<sim_env::Box2DWorld>();
-    world->loadWorld("/home/joshua/projects/planning_catkin/src/box2d_sim_env/test_data/test_env.yaml");
+    world->loadWorld("/home/joshua/projects/planning_catkin/src/planner_tests/data/box2d/worlds/free_world_1m_2s.yaml");
     sim_env::LoggerPtr logger = world->getLogger();
-    logger->logInfo("It works!");
+    logger->setLevel(sim_env::Logger::LogLevel::Debug);
+    logger->logInfo("Testing OraclePushPlanner using Box2DSimEnv");
     sim_env::Box2DWorldViewerPtr world_viewer = std::make_shared<sim_env::Box2DWorldViewer>(world);
     world_viewer->show(argc, argv);
     world_viewer->addCustomWidget(new widget::PushPlannerWidget(world), "PushPlanner");
