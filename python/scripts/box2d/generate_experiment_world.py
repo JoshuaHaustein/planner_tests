4a24abb5e499293b77742b00da0728ccdb90bf79
Isac Arnekvist <isac.arnekvist@gmail.com>
2018-01-08 09:13:27 +0100

    Change algo names

--- python/scripts/box2d/generate_experiment_world.py
+++ python/scripts/box2d/generate_experiment_world.py
@@ -10,7 +10,7 @@ if __name__ == '__main__':
                                                  'to \'experiment_world.yaml\' in the same folder as the template')
     parser.add_argument('--template_path', type=str, help="Path to template", required=True)
     parser.add_argument('--oracle_type', type=str, choices=['Learned', 'Human'], help="Which oracle to use", required=True)
-    parser.add_argument('--algorithm', type=str, choices=['CompleteSliceOracleRRT', 'SliceOracleRRT'], help='Which algorithm to use', required=True)
+    parser.add_argument('--algorithm', type=str, choices=['HybridActionRRT', 'SliceOracleRRT', 'OracleRRT'], help='Which algorithm to use', required=True)
     args = parser.parse_args()
     with open(args.template_path) as f:
         template = jinja2.Template(f.read())
