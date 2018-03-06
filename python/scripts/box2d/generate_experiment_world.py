d89e1b342b64859aeb35f7783b3831e7b72611c5
Joshua Haustein <haustein@kth.se>
2018-03-06 17:21:58 +0100

    Added shortcutting stuff to world generation script.

--- python/scripts/box2d/generate_experiment_world.py
+++ python/scripts/box2d/generate_experiment_world.py
@@ -13,6 +13,8 @@ if __name__ == '__main__':
     parser.add_argument('--algorithm', type=str, choices=['HybridActionRRT', 'SliceOracleRRT', 'OracleRRT'], help='Which algorithm to use', required=True)
     parser.add_argument('--num_control_samples', type=int, default=8, help='Required option for hybrid action RRT', required=False)
     parser.add_argument('--action_noise', type=float, default=0.03, help='Action noise, required for HybridActionRRT', required=False)
+    parser.add_argument('--shortcut_type', type=str, choices=['NoShortcut', 'LocalShortcut', 'NaiveShortcut', 'OracleShortcut', 'LocalOracleShortcut'], default='NoShortcut', help='Type of shortcut algorithm.', required=False)
+    parser.add_argument('--shortcut_timeout', type=float, default=5.0, help='Timeout for shortcutting algorithm in seconds', required=False)
     args = parser.parse_args()
     with open(args.template_path) as f:
         template = jinja2.Template(f.read())
@@ -23,6 +25,8 @@ if __name__ == '__main__':
     kwargs['num_control_samples'] = args.num_control_samples      # try [1, 4, 8]
 
     kwargs['action_noise'] = args.action_noise                    # For hybrid, try: [0, 0.5, 1.0]
+    kwargs['shortcut_type'] = args.shortcut_type
+    kwards['shortcut_timeout'] = args.shortcut_timeout
     rendered = template.render(
         oracle_type=args.oracle_type,
         algorithm=args.algorithm,
